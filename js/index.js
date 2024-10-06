be7cab810e4b593cba9f9dd04b2e064fe59e9add
hoaxed <thiagomartinez960@gmail.com>
2024-10-05 21:52:00 -0300

    Estilizado e interacciones del cliente completados, falta db

--- js/index.js
+++ js/index.js
@@ -1,4 +1,22 @@
-$(".speciment-image").click(()=> {
+$(document).click(function(event) {
+    if (!$(event.target).closest('.speciment-image').length) {
+        // Remover clase 'active' de todos los elementos .speciment-image
+        $(".speciment-image").removeClass('active');
+
+        // Agregar clase de animación para esconder y luego vaciar el contenido
+        $(".container-expanded").addClass('hide').one('animationend', function() {
+            $(".specimen-reader-container").empty();
+        });
+    }
+});
+
+$(".speciment-image").click(function(event) {
+    event.stopPropagation(); // Evitar que el evento de click se propague al documento
+
+    $(".speciment-image").removeClass('active');
+
+    $(this).addClass('active');
+    
     let container = `
     <div class='container-expanded'>
         <img src="media/Img-example.png" alt="Imagen de especimenes" class="image-expanded">
@@ -20,6 +38,16 @@ $(".speciment-image").click(()=> {
             </p>
         </div>
     </div>
-    `
-    $(".specimen-reader-container").html(container)
-})
+    `;
+    
+    $(".specimen-reader-container").html(container);
+
+    // Agregar clase de animación para mostrar
+    $(".container-expanded").addClass('show');
+
+    // Hacer scroll hasta el centro de la pantalla
+    $('html, body').animate({
+        scrollTop: $(".specimen-reader-container").offset().top - ($(window).height() / 2) + ($(".specimen-reader-container").height() / 2)
+    }, 1000); // Ajusta el tiempo de animación según sea necesario (1000 ms = 1 segundo)
+});
+// stars animation
